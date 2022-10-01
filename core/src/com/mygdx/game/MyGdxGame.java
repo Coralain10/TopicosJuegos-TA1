4d74f79a16b6c97616bc3d40297768f8ddabbbdd
Coralain <103149767+Cora42@users.noreply.github.com>
2022-09-30 23:11:03 -0500

    empty project MyGdx

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,37 +1,31 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	ShapeRenderer shapeRenderer;
-
+	SpriteBatch batch;
+	Texture img;
+	
 	@Override
 	public void create () {
-		shapeRenderer = new ShapeRenderer();
-		InitGame();
-	}
-
-	private void InitGame() {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-
-		shapeRenderer.end();
-		update();
-	}
-
-	private void update() {
-
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+		batch.dispose();
+		img.dispose();
 	}
 }
